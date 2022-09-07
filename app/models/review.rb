550cd39e39344867fedd49bad886239a93d2926e
Rick Marvin <marvin.rick@gmail.com>
2022-09-07 18:54:03 -0400

    Updated User Registration Edit formatting

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
     validates :stars, presence: true, numericality: { in: 0..5 }
     validates :title, presence: true
+    validates :book, presence: true
+    validates :user, presence: true
     belongs_to :user
 end
